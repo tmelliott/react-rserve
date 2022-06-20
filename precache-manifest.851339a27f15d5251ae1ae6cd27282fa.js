58e19ef5ceea33281a41c281a6a48c90c95b454b
Tom Elliott <tomelliottnz@gmail.com>
2022-06-20 13:28:12 +1200

    Updates

---
+++ precache-manifest.851339a27f15d5251ae1ae6cd27282fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cda4d3c433ac20d78e9e2ebbc30042f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3d66c8ad9c545598adbb",
+    "url": "./static/js/2.5b334ebe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.5b334ebe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6edb6ac81c0d7341cd5",
+    "url": "./static/js/main.d166f9f0.chunk.js"
+  },
+  {
+    "revision": "972e7ff94fe694eb3e11",
+    "url": "./static/js/runtime-main.1be1c99b.js"
+  }
+]);
